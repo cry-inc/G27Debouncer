6f4c242fa7638ac46faaf32553d8bb451e0a8bfc
cry-inc <cry-inc@users.noreply.github.com>
2015-04-20 20:58:50 +0200

    Reworked initialization and did some refactoring

--- G27Debouncer.cpp
+++ G27Debouncer.cpp
@@ -9,6 +9,7 @@
 #define VIRTUAL_DURATION	300
 #define VIRTUAL_GAP			400
 #define VIRTUAL_DELAY		5000
+#define VIRTUAL_JOYSTICK_ID	1u
 #define SDL_POLLING_TIME	10
 
 using namespace std;
@@ -18,7 +19,7 @@ bool checkForKeyIsDown(int key)
 	// Fetch key state.
 	SHORT tabKeyState = GetAsyncKeyState(key);
 	// Test high bit - if set, key was down when GetAsyncKeyState was called.
-	return (( 1 << 16 ) & tabKeyState);
+	return (1 << 16) & tabKeyState;
 }
 
 void exitWithErrorMsg(const char* errorMsg)
@@ -27,27 +28,41 @@ void exitWithErrorMsg(const char* errorMsg)
 	exit(EXIT_FAILURE);
 }
 
-int main(int argc, char* argv[])
+SDL_Joystick* getG27Joystick()
 {
 	if (SDL_Init(SDL_INIT_JOYSTICK))
 		exitWithErrorMsg("Could not initialize SDL Joystick subsystem!");
-	
-	int joysticks = SDL_NumJoysticks();
-	cout << "INFO: SDL found " << joysticks << " joysticks!" << endl;
-	if (joysticks < 1)
-		exitWithErrorMsg("Could not find a joystick!");
-
-	SDL_Joystick* j = SDL_JoystickOpen(0);
-	if (!j)
-		exitWithErrorMsg("SDL could not the input joystick!");
-
-	int buttons = SDL_JoystickNumButtons(j);
-	cout << "INFO: SDL found " << buttons << " buttons on the joystick!" << endl;
 
 	// Switch SDL to manual polling
 	int switched = SDL_JoystickEventState(SDL_IGNORE);
 	if (switched)
 		exitWithErrorMsg("SDL could not enable manual joystick polling!");
+	
+	int joystickNumber = SDL_NumJoysticks();
+	cout << "INFO: SDL found " << joystickNumber << " joystick(s)" << endl;
+	if (joystickNumber < 1)
+		exitWithErrorMsg("Could not find a joystick!");
+
+	for (int i=0; i<joystickNumber; ++i) {
+		SDL_Joystick* joystick = SDL_JoystickOpen(0);
+		if (!joystick)
+			continue;
+		int buttonNumber = SDL_JoystickNumButtons(joystick);
+		cout << "INFO: SDL found " << buttonNumber << " button(s) on joystick number " << i << endl;
+		if (buttonNumber == 23) { // The G27 reports exactly 23 buttons
+			cout << "INFO: Found G27 wheel at joystick number " << i << endl;
+			return joystick;
+		}
+		SDL_JoystickClose(joystick);
+	}
+
+	exitWithErrorMsg("Could not find the G27 joystick!");
+	return 0;
+}
+
+UINT getVirtualJoystick(UINT id)
+{
+	cout << "INFO: Trying to load virtual joystick at ID " << id << endl;
 
 	if (!vJoyEnabled())
 		exitWithErrorMsg("The vJoy driver is not running, install and start it first!");
@@ -56,14 +71,13 @@ int main(int argc, char* argv[])
 	if (!DriverMatch(&VerDll, &VerDrv))
 		exitWithErrorMsg("Version mismatch between the vJoy driver and interface DLL!");
 
-	UINT id = 1;
 	VjdStat status = GetVJDStatus(id);
 	if (status != VJD_STAT_FREE)
 		exitWithErrorMsg("The virtual joystick device is not free!");
 
-	int virtualButtons  = GetVJDButtonNumber(id);
-	cout << "INFO: The virtual joystick has " << virtualButtons << " buttons!" << endl;
-	if (virtualButtons < 2)
+	int buttonNumber  = GetVJDButtonNumber(id);
+	cout << "INFO: The virtual joystick has " << buttonNumber << " buttons" << endl;
+	if (buttonNumber < 2)
 		exitWithErrorMsg("The virtual joystick has not enough buttons!");
 
 	BOOL acquired = AcquireVJD(id);
@@ -74,29 +88,39 @@ int main(int argc, char* argv[])
 	if (!reset)
 		exitWithErrorMsg("The virtual joystick failed to reset!");
 
+	cout << "INFO: Virtual joystick found and initialized with ID " << id << endl;
+	return id;
+}
+
+int main(int argc, char* argv[])
+{
+	SDL_Joystick* g27 = getG27Joystick();
+	UINT vJoy = getVirtualJoystick(VIRTUAL_JOYSTICK_ID);
+
 	bool btnRight = false, btnLeft = false;
 	bool virtualRightOn = false, virtualLeftOn = false;
 	Uint32 virtualRightStart = 0, virtualLeftStart = 0;
 
+	cout << "INFO: Starting capture loop" << endl;
 	while (true) {
 		SDL_JoystickUpdate();
 		Uint32 now = SDL_GetTicks();
 
 		if (checkForKeyIsDown(VK_F6)) {
 			SDL_Delay(VIRTUAL_DELAY);
-			SetBtn(TRUE, id, 1);
+			SetBtn(TRUE, vJoy, 1);
 			SDL_Delay(VIRTUAL_DURATION);
-			SetBtn(FALSE, id, 1);
+			SetBtn(FALSE, vJoy, 1);
 		}
 
 		if (checkForKeyIsDown(VK_F5)) {
 			SDL_Delay(VIRTUAL_DELAY);
-			SetBtn(TRUE, id, 2);
+			SetBtn(TRUE, vJoy, 2);
 			SDL_Delay(VIRTUAL_DURATION);
-			SetBtn(FALSE, id, 2);
+			SetBtn(FALSE, vJoy, 2);
 		}
 
-		bool right = SDL_JoystickGetButton (j, 4);
+		bool right = SDL_JoystickGetButton (g27, 4);
 		if (right != btnRight) {
 			btnRight = right;
 			cout << "INFO: Right hardware button changed to " << btnRight << endl;
@@ -104,11 +128,11 @@ int main(int argc, char* argv[])
 			if (sinceLastStart > VIRTUAL_GAP && btnRight) {
 				virtualRightOn = true;
 				virtualRightStart = now;
-				SetBtn(TRUE, id, 1);
+				SetBtn(TRUE, vJoy, 1);
 			}
 		}
 
-		bool left = SDL_JoystickGetButton (j, 5) || checkForKeyIsDown(VK_F5);
+		bool left = SDL_JoystickGetButton (g27, 5);
 		if (left != btnLeft) {
 			btnLeft = left;
 			cout << "INFO: Left hardware button changed to " << btnLeft << endl;
@@ -116,23 +140,23 @@ int main(int argc, char* argv[])
 			if (sinceLastStart > VIRTUAL_GAP && btnLeft) {
 				virtualLeftOn = true;
 				virtualLeftStart = now;
-				SetBtn(TRUE, id, 2);
+				SetBtn(TRUE, vJoy, 2);
 			}
 		}
 
 		if (virtualRightOn && now - virtualRightStart > VIRTUAL_DURATION) {
 			virtualRightOn = false;
-			SetBtn(FALSE, id, 1);
+			SetBtn(FALSE, vJoy, 1);
 		}
 
 		if (virtualLeftOn && now - virtualLeftStart > VIRTUAL_DURATION) {
 			virtualLeftOn = false;
-			SetBtn(FALSE, id, 2);
+			SetBtn(FALSE, vJoy, 2);
 		}
 
 		SDL_Delay(SDL_POLLING_TIME);
 	}
 
-	SDL_JoystickClose(j);
+	SDL_JoystickClose(g27);
 	return EXIT_SUCCESS;
 }
